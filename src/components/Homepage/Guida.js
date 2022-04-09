f7e148369302b91ca70417b12864d3c74c34f67b
unknown <lucapaduano09@outlook.it>
2022-04-09 22:47:08 +0200

    si

--- src/components/Homepage/Guida.js
+++ src/components/Homepage/Guida.js
@@ -2,17 +2,20 @@ import React from "react";
 import "../../styles/Guida.css";
 import Bounce from "react-reveal/Bounce";
 import { Link } from "react-router-dom";
+
 const Guida = (props) => {
   const capitoli = props.capitoli;
   const prova = props.prova;
   return (
     <div className="guida-container">
       <div className="guida-wrapper">
-          <div className="guida-articolo-container">
+        <Bounce>
+          <div className="guida-articolo-container"style={{backgroundImage:`url("../../public/capitolo1.jpg")`}}>
             <Link to={"/come-iniziare"}>
               Come Iniziare
             </Link>
           </div>
+        </Bounce>
           <div className="guida-articolo-container">
             <a href="">Token</a>
           </div>
