8f55d75fb185262e916c9f6509188305188c9076
unknown <lucapaduano09@outlook.it>
2022-04-14 15:35:10 +0200

    fixed notice

--- src/components/News/NewsHome.js
+++ src/components/News/NewsHome.js
@@ -7,7 +7,15 @@ import Animazione from "./loading.json";
 
 const NewsHome = () => {
   const [news, setNews] = useState([]);
+  const [copia, setCopia] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  //variabili controllo filter
+  const [appro, setAppro] = useState(false);
+  const [notizia, setNotizia] = useState(false);
+  const approfondimenti = news.filter((el,index)=> {return (el.tipologia === "approfondimento")});
+  const notizie = news.filter((el,index)=> {return (el.tipologia === "notizia")})
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -16,6 +24,26 @@ const NewsHome = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+  
+  const handleApprClick = (copia) => {
+    // console.log(copia)
+    setNews(copia);
+    let filtered = [] ;
+    filtered = copia.filter((el,index)=> {return (el.tipologia === "approfondimento")})
+    // console.log(filtered)
+    console.log(news)
+    setNews(filtered);
+  }
+  const handleNewsClick = (copia) => {
+    // console.log(copia);
+    setNews(copia);
+    let filtered = copia.filter((el,index)=> {return (el.tipologia === "notizia")})
+    console.log(news)
+    setNews(filtered);
+  }
+  const handleTutteClick = (copia) => {
+    setNews(copia);
+  }
   useEffect(() => {
     const getNews = async () => {
       setLoading(true);
@@ -37,22 +65,26 @@ const NewsHome = () => {
       //ordino in modo descrescente l'array
       result.sort((a, b) => a - b).reverse()
       setNews(result);
+      setCopia(result);
       setLoading(false);
     };
     getNews();
-  }, [news.length]);
+  },[]);
 
   return (
     <div className="news-home-container">
       <div className="filter-container">
         <div className="tipo-filter-container">
-          <button>
+          <button onClick={() => handleApprClick(copia)}>
             <div>Approfondimenti</div>
           </button>
-          <button>
+          <button onClick={() => handleNewsClick(copia)}>
             <div>
               Notizie  
             </div>
+          </button>
+          <button onClick={() => handleTutteClick(copia)}>
+            <div>tutte</div>
           </button>
         </div>
         <div className="search-bar-container">
@@ -61,37 +93,38 @@ const NewsHome = () => {
         </div>
       </div>
       <div className="news-list-container">
-        {news.map((n) => (
-          <Link to={"/news/"+ n._id}>
-            {loading === true ? (
-              <div>
-                <p style={{ color: "white" }}>Stiamo caricando le news, grazie per la pazienza</p>
-                <div>
-                  <Lottie
-                    style={{ height: "200px", width: "200px" }}
-                    options={defaultOptions}
-                  />
-                </div>
-              </div>
-            ) : (
-              <div className="single-list-wrapper">
-                <div className="single-list-new">
-                  <img src={n.image} alt="news" />
-                  <div className="single-list-new-text">
-                    <h1>{n.titolo}</h1>
-                    <div className="single-new-category">
-                      {n.categorie.map((el,index)=>(
-                        <p className='single-category'key={index}>{el}</p>
-                      ))}
+        {loading === true ? (
+                    <div>
+                      <p style={{ color: "white" }}>Stiamo caricando le news, grazie per la pazienza</p>
+                      <div>
+                        <Lottie
+                          style={{ height: "200px", width: "200px" }}
+                          options={defaultOptions}
+                        />
+                      </div>
                     </div>
-                    <h3>{n.descrizione}</h3>
-                  </div>
-                </div>
-                <div className="separator"></div>
-              </div>
-            )}
-          </Link>
-        ))}
+                  ) : (
+                  news.map((n) => (
+                <Link to={"/news/"+ n._id}>
+                    <div className="single-list-wrapper">
+                      <div className="single-list-new">
+                        <img src={n.image} alt="news" />
+                        <div className="single-list-new-text">
+                          <h1>{n.titolo}</h1>
+                          <div className="single-new-category">
+                            {n.categorie.map((el,index)=>(
+                              <p className='single-category'key={index}>{el}</p>
+                            ))}
+                          </div>
+                          <h3>{n.descrizione}</h3>
+                        </div>
+                      </div>
+                      <div className="separator"></div>
+                    </div>
+                </Link>)
+                  
+            ))
+        }
       </div>
     </div>
   );
